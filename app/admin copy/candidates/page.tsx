5cca7da92b12011309af4ef6acbfba1966017be9
Shivansh <shivanshsaxena2910@gmail.com>
2025-05-22 22:05:13 +0530

    2

--- app/admin copy/candidates/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CandidatesTable } from "@/components/admin/candidates/candidates-table";
-
-export default function ApplicationsPage() {
-  return (
-    <div className="flex flex-col gap-6">
-      <CandidatesTable />
-    </div>
-  );
-}
--- app/admin copy/jobs/page.tsx
+++
@@ -1,9 +0,0 @@
-import { JobsTable } from "@/components/admin/jobs/jobs-table"
-
-export default function JobsPage() {
-  return (
-    <div className="flex flex-col gap-6">
-      <JobsTable />
-    </div>
-  )
-}
